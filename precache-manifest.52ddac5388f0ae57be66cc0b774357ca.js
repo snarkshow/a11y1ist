a0f0e72f242d7f609a9eccb056c1380a812dd3c1
snarkshow <laura.hw.chuang@gmail.com>
2019-06-03 10:48:24 -0400

    Updates

---
+++ precache-manifest.52ddac5388f0ae57be66cc0b774357ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e936955a490ea00fc6c5d9a625e34b95",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "0bfb9b44a6f454c7721b",
+    "url": "/repository-name/static/css/main.7f5da9b5.chunk.css"
+  },
+  {
+    "revision": "93260b9c0c5f133e8517",
+    "url": "/repository-name/static/js/2.edb0a695.chunk.js"
+  },
+  {
+    "revision": "0bfb9b44a6f454c7721b",
+    "url": "/repository-name/static/js/main.e8668c1b.chunk.js"
+  },
+  {
+    "revision": "6e2fc4b7d77a9c53df47",
+    "url": "/repository-name/static/js/runtime~main.f7634681.js"
+  },
+  {
+    "revision": "33c29231470a87093dab3374eebc459a",
+    "url": "/repository-name/static/media/clipboard2.33c29231.svg"
+  },
+  {
+    "revision": "f76a17f79139a0e90e5b829a26e34727",
+    "url": "/repository-name/static/media/h1logo.f76a17f7.svg"
+  }
+]);
